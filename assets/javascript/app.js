f332ef97149f32faa4d587ef7cf2202f1cb7e83d
Justin Lampe <pierrelampre@gmail.com>
2019-10-26 15:48:39 -0400

    attempting bug fix, switching API url from http to https

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,9 @@
-var topics = ["hamsters", "bugs bunny", "rick and morty", "dancing"];
+var topics = ["capybara", "bugs bunny", "rick and morty", "dancing"];
 
       function displaygifInfo() {
 
         var gif = $(this).text();
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gif + "&api_key=r3h8jdHJHdZEPmBDKE12DCTXynTZ8f1s&limit=10";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gif + "&api_key=r3h8jdHJHdZEPmBDKE12DCTXynTZ8f1s&limit=10";
         newDiv = $("<div>");
 
          $("#gifs-view").empty();
@@ -17,12 +17,8 @@ var topics = ["hamsters", "bugs bunny", "rick and morty", "dancing"];
 
         for (let i = 0; i < response.data.length; i++) {
             
-            newDiv.append("<span class='gifspan'><p>Rating: " + response.data[i].rating + "</p><img src='" + response.data[i].images.downsized_still.url + "'id='displayed-gif'></span>");
+            newDiv.append("<span class='gifspan'><p>Rating: " + response.data[i].rating + "</p><img src='" + response.data[i].images.downsized.url + "'id='displayed-gif'></span>");
             $("#gifs-view").prepend(newDiv);
-
-            $("#displayed-gif").on("click", function() {
-                $(newDiv).html("<span class='gifspan'><p>Rating: " + response.data[i].rating + "</p><img src='" + response.data[i].images.downsized.url + "'id='displayed-gif'></span>")
-            });
         }
           
              console.log(response);
